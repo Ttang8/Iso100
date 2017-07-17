66aa01ab1e6581ab5526a6d801952c996e3003f1
Terrence Tang <thtang28@gmail.com>
2017-07-17 11:18:55 -0700

    Add backend auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+    else
+      render json: ['Invalid username/password']
+    end
+  end
+
+  def destroy
+    logout
+  end
+end
