964359776f7c2d5d34d501e2e6ca0b592e9b7ff2
Terrence Tang <thtang28@gmail.com>
2017-07-17 12:16:25 -0700

    Work on frontend auth

--- frontend/iso_100.jsx
+++ frontend/iso_100.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as APIUtil from './util/session_api_util';
+
+window.login = APIUtil.login;
+window.logout = APIUtil.logout;
+window.signup = APIUtil.signup;
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
